5bc33f4e7ad8ecce764e943c096c384e87df4e53
shayan1234554321 <kshayan091@gmail.com>
2023-02-23 02:51:12 +0500

    Complete and delete all interactions added

---
+++ src/interactions.js
@@ -0,0 +1,26 @@
+import { Save } from './local-storage.js';
+
+const handleComplete = (tasks, index, checkValue, checked) => {
+  tasks.array[index].completed = checkValue;
+  checked();
+  Save(tasks.array);
+};
+
+const clearAllComplete = (tasks, addAllTasksToHTML) => {
+  const tempTasks = [];
+  let index = 0;
+  for (let i = 0; i < tasks.array.length; i += 1) {
+    if (tasks.array[i].completed === false) {
+      tempTasks.push({ ...tasks.array[i], index });
+      index += 1;
+    }
+  }
+  tasks.array = tempTasks;
+  Save(tasks.array);
+  addAllTasksToHTML();
+};
+
+export {
+  handleComplete,
+  clearAllComplete,
+};
